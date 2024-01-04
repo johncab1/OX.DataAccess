756ab9c4a81a98d791cbbe15b06438797e13592e
Johncab1 <jonatan.orc@gmail.com>
2024-01-03 22:06:41 -0600

    Agregar archivos de proyecto.

---
+++ OX.DataAccess/Data.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.Data.SqlClient;
+
+namespace OX.Data
+{
+    public class Data
+    {
+        public IEnumerable<T> ExecSp<T>(string storedProcedure, string connectionString, SqlParameter[] parameters, Func<SqlDataReader, T> body)
+        {
+            List<T> results = new List<T>();
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                //SqlCommand command = connection.CreateCommand(storedProcedure, connectionString);
+                SqlCommand command = new SqlCommand(storedProcedure, connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddRange(parameters);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    results.Add(body(reader));
+                }
+                reader.Close();
+            }
+            return results;
+        }
+    }
+}
