7ecbf87ef45a176b2dfc9c4216994d865f1c6a6b
Johncab1 <jonatan.orc@gmail.com>
2024-01-04 23:53:09 -0600

    se agrega conversion de entidad a parametros sql

--- OX.DataAccess/Data.cs
+++ OX.DataAccess/Data.cs
@@ -1,28 +1,69 @@
 ﻿
 using Microsoft.Data.SqlClient;
+using System.ComponentModel;
+using System.Data;
+using System.Reflection;
 
-namespace OX.Data
+namespace OX.DataAccess
 {
-    public class Data
+    public static class Data
     {
-        public IEnumerable<T> ExecSp<T>(string storedProcedure, string connectionString, SqlParameter[] parameters, Func<SqlDataReader, T> body)
+        public static string OXConnectionStr = "";
+
+        public static IEnumerable<T> ExecSp<T>(string storedProcedure, SqlParameter[] parameters, Func<SqlDataReader, T> body)
         {
             List<T> results = new List<T>();
 
-            using (SqlConnection connection = new SqlConnection())
+            using (SqlConnection connection = new SqlConnection(OXConnectionStr))
             {
+                connection.Open();
                 //SqlCommand command = connection.CreateCommand(storedProcedure, connectionString);
                 SqlCommand command = new SqlCommand(storedProcedure, connection);
                 command.CommandType = System.Data.CommandType.StoredProcedure;
                 command.Parameters.AddRange(parameters);
                 SqlDataReader reader = command.ExecuteReader();
+
                 while (reader.Read())
                 {
                     results.Add(body(reader));
                 }
                 reader.Close();
+                connection.Close();
             }
             return results;
         }
+
+        public static SqlParameter[] GetOXParameters<T>(T entity) where T : class
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            PropertyInfo[] properties = typeof(T).GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                SqlDbType DbType;
+
+                SqlParameter parameter = new()
+                {
+                    ParameterName = "@" + property.Name,
+                    Direction = ParameterDirection.Input,
+                    Value = property.GetValue(entity)
+                };
+                if (property.PropertyType.Equals(typeof(int)))
+                    parameter.SqlDbType = SqlDbType.Int;
+                if (property.PropertyType.Equals(typeof(string)))
+                    parameter.SqlDbType = SqlDbType.VarChar;
+                if (property.PropertyType.Equals(typeof(bool)))
+                    parameter.SqlDbType = SqlDbType.Bit;
+
+                parameters.Add(parameter);
+
+            }
+
+            return parameters.ToArray();
+
+        }
     }
+
+       
+    
 }
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# OX.DataAccess
+# OX.DataAccess 1.0.4
+
+Implementación de ejemplo
+
+    
+    public Class DB
+    {
+        public DB()
+        {
+            Data.OXConnectionStr = MyConnectionString;
+        }
+
+        public void New(Person person)
+        {            
+            var result = Data.ExecSp<Response>("dbo.StoredProcedureName",
+            Data.GetOXParameters(person),  //los parametros se toman de cada propiedad de la entidad person
+            reader =>
+            {
+                return new Response
+                {
+                    Code = reader["Code"].ToString(),
+                    Message = reader["Message"].ToString()
+                };
+            });
+        }
+
+    }
